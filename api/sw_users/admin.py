6ed91d13525db51174bb12ab80d4d0cd969d33a2
lzambrano18 <lezy17@gmail.com>
2016-11-29 17:28:20 -0400

    H005-ListService: List Service

--- api/sw_users/admin.py
+++ api/sw_users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.gis import admin
 from sw_users import models
+from django.utils.translation import ugettext as _
 
 
 class AccountInline(admin.StackedInline):
@@ -15,3 +16,16 @@ class ClientAdmin(admin.ModelAdmin):
 @admin.register(models.ServiceAccount)
 class ServiceAdmin(admin.OSMGeoAdmin):
     inlines = (AccountInline,)
+    list_display = ('get_full_name', 'get_username', 'identity_card', 'phone', 'identity_check')
+    list_editable = ('identity_check',)
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('useraccount__first_name', 'useraccount__last_name', 'useraccount__username' , 'identity_card',)
+
+    def get_full_name(self, obj):
+        return '{}'.format(obj.useraccount.get_full_name())
+
+    def get_username(self, obj):
+        return '{}'.format(obj.useraccount.username)
+
+    get_full_name.short_description = _('full Name')
+    get_username.short_description = _('username')
