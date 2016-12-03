1d3d9ec78b40b9e73485bd5fedfff8a66df00215
lzambrano18 <lezy17@gmail.com>
2016-12-02 21:02:41 -0400

    Fix-GetData: Change getdata

--- api/sw_vehicles/api.py
+++ api/sw_vehicles/api.py
@@ -4,26 +4,65 @@ from sw_shipments.models import ShipmentType
 from sw_vehicles.models import Model, PackageType, VehicleCategory
 
 
-class GetDataView(views.APIView):
+class GetPackageTypeView(views.APIView):
 
     permission_classes = (permissions.AllowAny,)
 
     def get(self, request, format=None):
         base = {}
-        base['packagetype'] = {
-            obj.id: obj.name
-            for obj in PackageType.objects.all()
-        }
-        base['vehiclecategory'] = {
-            obj.id: obj.name
-            for obj in VehicleCategory.objects.all()
-        }
-        base['model'] = {
-            obj.id: obj.name
-            for obj in Model.objects.all()
-        }
-        base['shipmenttype'] = {
-            obj.id: obj.name
-            for obj in ShipmentType.objects.all()
-        }
+        base['packagetype'] = []
+
+        for obj in PackageType.objects.all():
+            json = {}
+            json['id'] = obj.id
+            json['value'] = obj.name
+            base['packagetype'].append(json)
+        return Response(base)
+
+
+class GetVehicleCategoryView(views.APIView):
+
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        base = {}
+        base['vehiclecategory'] = []
+
+        for obj in VehicleCategory.objects.all():
+            json = {}
+            json['id'] = obj.id
+            json['value'] = obj.name
+            base['vehiclecategory'].append(json)
+        return Response(base)
+
+
+class GetModelView(views.APIView):
+
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        base = {}
+        base['model'] = []
+
+        for obj in Model.objects.all():
+            json = {}
+            json['id'] = obj.id
+            json['value'] = obj.name
+            base['model'].append(json)
+        return Response(base)
+
+
+class GetShipmentTypeView(views.APIView):
+
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        base = {}
+        base['shipmenttype'] = []
+
+        for obj in ShipmentType.objects.all():
+            json = {}
+            json['id'] = obj.id
+            json['value'] = obj.name
+            base['shipmenttype'].append(json)
         return Response(base)
--- api/sw_vehicles/urls.py
+++ api/sw_vehicles/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from sw_vehicles.api import GetDataView
+from sw_vehicles.api import GetModelView, GetPackageTypeView, GetShipmentTypeView, GetVehicleCategoryView
 
 urlpatterns = [
-    url(r'^getdata/$', GetDataView.as_view(), name='getdata'),
+    url(r'^getpackagetype/$', GetPackageTypeView.as_view(), name='getpackagetype'),
+    url(r'^getvehiclecategory/$', GetVehicleCategoryView.as_view(), name='getvehiclecategory'),
+    url(r'^getmodel/$', GetModelView.as_view(), name='getmodel'),
+    url(r'^getshipmenttype/$', GetShipmentTypeView.as_view(), name='getshipmenttype'),
 ]
