611d7210eb03df550d29623e285565bbda0a0155
Edwar Baron <edwar.baron@gmail.com>
2016-12-04 18:28:06 -0500

    (fix_permissions) Corrections

--- api/sw_users/mixins.py
+++ api/sw_users/mixins.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from rest_framework import permissions
-from sw_users.permissions import IsAdminOrAccountOwner, IsClientAccountOwner, IsServiceAccountOwner
+from sw_users.permissions import IsAdminOrAccountOwner
 
 
 class DefaultCRUDPermissions(object):
@@ -22,47 +22,5 @@ class DefaultCRUDPermissions(object):
 
         if self.action == 'list':
             return [permissions.IsAdminUser()]
-
-        return [permission() for permission in self.permission_classes]
-
-
-class ClientCRUDPermissions(object):
-    """
-    Mixin to verify if the user can access to a method through a web service
-    """
-
-    def get_permissions(self):
-        """
-        Get permissions
-        """
-        if self.action == 'create':
-            return [IsClientAccountOwner()]
-
-        if self.action in ['update', 'partial_update', 'destroy', 'retrieve']:
-            return [IsClientAccountOwner()]
-
-        if self.action == 'list':
-            return [IsClientAccountOwner]
-
-        return [permission() for permission in self.permission_classes]
-
-
-class ServiceCRUDPermissions(object):
-    """
-    Mixin to verify if the user can access to a method through a web service
-    """
-
-    def get_permissions(self):
-        """
-        Get permissions
-        """
-        if self.action == 'create':
-            return [IsServiceAccountOwner()]
-
-        if self.action in ['update', 'partial_update', 'destroy', 'retrieve']:
-            return [IsServiceAccountOwner()]
-
-        if self.action == 'list':
-            return [IsServiceAccountOwner]
 
         return [permission() for permission in self.permission_classes]
